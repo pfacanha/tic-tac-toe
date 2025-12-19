80486a5473412fe3fde2ffefaebaccd1af4287a5
pfacanha <pfacanha.dev@gmail.com>
2025-12-19 11:00:12 -0800

    Add more functions to the project

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 // create a Gameboard object as an IIFE
-const Gameboard = (function () {
-  let board = [];
+const Game = (function () {
+  let gameboard = [];
+  let players = [];
+
+  function init() {
+    console.log("You just started the game!");
+  }
+
+  function createUser(name) {
+    players.push(name);
+    return name;
+  }
+
+  function displayPlayers() {
+    for (let i = 0; i < players.length; ++i) {
+      console.log(`Player ${i + 1} is ${players[i]}`);
+    }
+  }
+
+  return {
+    init,
+    createUser,
+    displayPlayers,
+  };
 })();
+
+Game.init();
+const player1 = Game.createUser("Pedro");
+console.log(player1);
+Game.displayPlayers();
