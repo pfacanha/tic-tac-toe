41712d1188db7f917660ef367fb47004f8077da1
pfacanha <pfacanha.dev@gmail.com>
2025-12-20 10:39:58 -0800

    review some concepts of IIFE before proceeding to next step

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const Game = (function () {
   let gameboard = [];
   let players = [];
+  let count = 0;
 
   function init() {
     console.log("You just started the game!");
-    return function displayGame() {
+    console.log(`${count} rounds were played so far!`);
+    function displayGame() {
       if (players.length == 0) {
         console.log("No players in the game yet");
       } else {
@@ -13,21 +15,25 @@ const Game = (function () {
           console.log(`Player ${i + 1} is ${players[i]}`);
         }
       }
-    };
+    }
+    displayGame();
   }
 
   function createUser(name) {
-    const username = name;
-    players.push(username);
+    players.push(name);
     return name;
   }
+
+  const playRound = () => count++;
 
   return {
     init,
     createUser,
-    displayPlayers,
+    playRound,
   };
 })();
 
 Game.init();
 const player1 = Game.createUser("Pedro");
+console.log(`${player1} is the player`);
+console.log(Game.count);
