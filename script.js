4d66589809b592e63a215d94355b1b60765153ac
pfacanha <pfacanha.dev@gmail.com>
2025-12-22 21:31:59 -0800

    just started from scratch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // start project "Tic-Tac-Toe" from scratch
+const Gameboard = {
+  board: [],
+};
+
+const players = [];
+
+function createPlayer(name) {
+  return {
+    name,
+  };
+}
+
+function addPlayer(player) {
+  players.push(player);
+}
+
+const player1 = createPlayer("Pedro");
